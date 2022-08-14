ca8482f4e838ce9b07d1176deca53c2ecc78c7de
Matias Schmidt <matias.schmidt@outlook.es>
2022-08-14 11:31:34 -0300

    responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './css/contact.css'
 import {HiOutlineMail} from "react-icons/hi"
+import {BsLinkedin, BsGithub, BsInstagram} from "react-icons/bs"
+
 
 export default class Contact extends Component {
   render() {
@@ -10,6 +12,11 @@ export default class Contact extends Component {
               <span className='titulo-span'> Contacto </span>
               <div className="row">
                 <a href='mailto:matias.schmidt@oulook.es' isExternal target='_blank' rel='noreferrer'> <HiOutlineMail className='mail'/> </a>
+              </div>
+              <div className='socials-header-contact'>
+                <a className='social' href='https://www.linkedin.com/in/matias-schmidt1/' target='_blank' rel='noreferrer'> <BsLinkedin/> </a>
+                <a className='social' href='https://github.com/mschmidt17' target='_blank' rel='noreferrer'> <BsGithub/> </a>
+                <a className='social' href='https://www.instagram.com/mati_schmidt1/' target='_blank' rel='noreferrer'> <BsInstagram/> </a>
               </div>
           </div>
       </section>
